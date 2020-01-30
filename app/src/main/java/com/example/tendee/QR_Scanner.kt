034ab741feda4709bfe90b1cfecab082d5b3bc48
William Padron <padron1999@yahoo.com>
2020-01-30 13:16:34 -0500

    Image and Button Constraints

---
+++ app/src/main/java/com/example/tendee/QR_Scanner.kt
@@ -0,0 +1,14 @@
+package com.example.tendee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QR_Scanner : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr__scanner)
+    }
+
+
+}
