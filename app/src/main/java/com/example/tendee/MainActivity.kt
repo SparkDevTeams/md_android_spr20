ccbfbfdfa864387cafb82271c2435094081aff59
SUNTheBaer <padron1999@yahoo.com>
2020-02-13 12:01:30 -0500

    fotoapparat implemented

--- app/src/main/java/com/example/tendee/MainActivity.kt
+++ app/src/main/java/com/example/tendee/MainActivity.kt
@@ -1,43 +1,22 @@
 package com.example.tendee
 
-import android.app.Activity
-import android.content.Intent
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import kotlinx.android .synthetic.main.activity_qr__scanner.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import io.fotoapparat.Fotoapparat
+import io.fotoapparat.log.logcat
+import io.fotoapparat.log.loggers
+import io.fotoapparat.parameter.ScaleType
+import io.fotoapparat.selector.back
+import io.fotoapparat.view.CameraView
 
 class MainActivity : AppCompatActivity() {
 
     val CAMERA_REQUEST_CODE = 0;
     val permissions = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
     var fotoapparat: Fotoapparat? = null
-
-    /*override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        cameraButton.setOnClickListener{
-            val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if(callCameraIntent.resolveActivity(packageManager) != null) {
-                startActivityForResult(callCameraIntent, CAMERA_REQUEST_CODE)
-            }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        when(requestCode){
-            CAMERA_REQUEST_CODE -> {
-                if(resultCode == Activity.RESULT_OK && data != null){
-                    photoimageView.setImageBitmap(data.extras.get("data") as Bitmap)
-                }
-            }
-            else -> {
-                Toast.makeText(this, "Unrecognized request code", LENGTH_SHORT).show()
-            }
-        }
-    }*/
 
     private fun hasNoPermissions(): Boolean{
         return ContextCompat.checkSelfPermission(this,
