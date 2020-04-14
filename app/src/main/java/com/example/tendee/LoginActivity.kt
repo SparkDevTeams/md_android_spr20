8449ee5aae7ef9ed41c40d115967f79c7403840d
Dawson Neves de Araujo <dawsonaraujo13@gmail.com>
2020-04-14 08:54:08 -0400

    merge

--- app/src/main/java/com/example/tendee/LoginActivity.kt
+++ app/src/main/java/com/example/tendee/LoginActivity.kt
@@ -3,7 +3,7 @@ package com.example.tendee
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
